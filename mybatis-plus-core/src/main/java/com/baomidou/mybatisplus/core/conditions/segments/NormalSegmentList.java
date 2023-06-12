31a641b01261f9802d29c06d50cf990e32d52b32
Lyman Mccuin <135798357+MaxWellu123@users.noreply.github.com>
2023-06-12 12:01:10 +0800

    增加transformList注释

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/segments/NormalSegmentList.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/segments/NormalSegmentList.java
@@ -43,14 +43,18 @@ public class NormalSegmentList extends AbstractISegmentList {
     protected boolean transformList(List<ISqlSegment> list, ISqlSegment firstSegment, ISqlSegment lastSegment) {
         if (list.size() == 1) {
             /* 只有 and() 以及 or() 以及 not() 会进入 */
+            // 只有and() 或者 or 或者 not进入这里的方法
             if (!MatchSegment.NOT.match(firstSegment)) {
                 //不是 not
+                // 如果是第一次进入这里的话，那么代表用户直接使用了not，或者or或者and，不加入到集合中
                 if (isEmpty()) {
                     //sqlSegment是 and 或者 or 并且在第一位,不继续执行
                     return false;
                 }
+
                 boolean matchLastAnd = MatchSegment.AND.match(lastValue);
                 boolean matchLastOr = MatchSegment.OR.match(lastValue);
+                // 判断上一次是否是and或者or
                 if (matchLastAnd || matchLastOr) {
                     //上次最后一个值是 and 或者 or
                     if (matchLastAnd && MatchSegment.AND.match(firstSegment)) {
@@ -59,20 +63,30 @@ public class NormalSegmentList extends AbstractISegmentList {
                         return false;
                     } else {
                         //和上次的不一样
+                        // 进入到这里说明上一次是and，这次是or
+                        // 或者上一次是or，这次是and
+                        // 删除最后上一次的值，使用最新的值
                         removeAndFlushLast();
                     }
                 }
             } else {
+                // 代表这次是not了
+                // 等到下次的时候直接添加到集合里面
                 executeNot = false;
                 return false;
             }
         } else {
+            // 判断是否是普通的添加
             if (MatchSegment.APPLY.match(firstSegment)) {
                 list.remove(0);
             }
+            // 如果上一次既不是and，也不是or，而且集合里面有值，
+            // 那么添加上默认的and
             if (!MatchSegment.AND_OR.match(lastValue) && !isEmpty()) {
                 add(SqlKeyword.AND);
             }
+
+            // 代表上一次是not，这里添加上not符号
             if (!executeNot) {
                 list.add(0, SqlKeyword.NOT);
                 executeNot = true;
